8208ee4dc4ce04d328ab1e5011b59d981d907d64
sadness_ojisan <sadness.ojisan@gmail.com>
2020-03-28 01:38:54 +0900

    chore: reduxをhooks化

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,36 +1,18 @@
 import * as React from "react";
-import { connect } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
 import { actionCreators } from "../redux/modules/app";
 
-class AppBase extends React.Component<any, any> {
-  componentDidMount() {
-    this.props.fetch("piyoooo");
-  }
-
-  render() {
-    return (
-      <div>
-        hello, {JSON.stringify(this.props.app)}{" "}
-        <button
-          onClick={() => {
-            this.props.fetch("hogee");
-          }}
-        >
-          fetch data
-        </button>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state: any) => ({
-  app: state.app
-});
-
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    fetch: (query: any) => dispatch(actionCreators.fetchDataStart(query))
-  };
+export const App: React.FC = () => {
+  const state = useSelector((state: any) => ({ app: state.app }));
+  const dispatch = useDispatch();
+  return (
+    <div>
+      hi, {JSON.stringify(state.app)},{" "}
+      <button
+        onClick={() => dispatch(actionCreators.fetchDataStart("piyooooo"))}
+      >
+        fetch
+      </button>
+    </div>
+  );
 };
-
-export const App = connect(mapStateToProps, mapDispatchToProps)(AppBase);
